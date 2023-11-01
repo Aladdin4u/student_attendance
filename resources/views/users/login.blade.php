67f7a8b879c9dda6ed20a4a6a11e549a6544d8f1
aladdin4u <taofikakinkumi@gmail.com>
2023-11-01 10:19:09 +0100

    add style to login and sign up pages

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,46 +1,101 @@
-<h1>Login</h1>
-<form method="POST" action="/login">
-            @csrf
-            <div class="mb-6">
-                <label for="email" class="inline-block text-lg mb-2">Email</label>
-                <input type="email" class="border border-gray-200 rounded p-2 w-full" name="email" placeholder="Other Names" value="{{old('email')}}" />
-                @error('email')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
-            </div>
+<!DOCTYPE html>
+<html lang="en">
 
-            <div class="mb-6">
-                <label for="role" class="inline-block text-lg mb-2">Role</label>
-                <select name="role" id="role" class="border border-gray-200 rounded p-2 w-full">
-                    <option value="">--Choose role --</option>
-                    <option value="admin">Admin</option>
-                    <option value="lecturer">Lecturer</option>
-                </select>
-                @error('role')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
-            </div>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	@vite('resources/css/app.css')
+	<title>Login</title>
+</head>
 
-            <div class="mb-6">
-                <label for="password" class="inline-block text-lg mb-2">
-                    Password
-                </label>
-                <input type="password" class="border border-gray-200 rounded p-2 w-full" name="password" placeholder="password" value="{{old('password')}}" />
-                @error('password')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
-            </div>
+<body class="h-full">
+	<main class="flex min-h-full flex-col justify-center px-6 py-8 lg:px-8">
+		<div class="sm:mx-auto sm:w-full sm:max-w-sm border rounded-xl py-12 px-6">
 
-            <div class="mb-6">
-                <button class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
-                    Login
-                </button>
-            </div>
+			<div class="sm:mx-auto sm:w-full sm:max-w-sm">
+				<img class="mx-auto h-16 w-auto" src={{asset("avatar.png")}} alt="Your Company">
+				<h2 class="mt-2 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Login</h2>
+			</div>
 
-            <div class="mt-8">
-                <p>
-                    Don't have an account?
-                    <a href="/register" class="text-laravel">Sign Up</a>
-                </p>
-            </div>
-        </form>
+			<div class="mt-4 sm:mx-auto sm:w-full sm:max-w-sm">
+				<form class="space-y-6" action="/login" method="POST">
+					@csrf
+					<div>
+						<label for="role" class="block text-sm font-medium leading-6 text-gray-900">Role</label>
+						<div class="mt-2">
+							<select name="role" id="role" class="block w-full rounded-md border-0 py-1.5 px-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6">
+								<option value="">-- select role --</option>
+								<option value="admin">Admin</option>
+								<option value="lecturer">Lecturer</option>
+							</select>
+						</div>
+						@error('role')
+						<div class="flex items-center p-4 mt-1 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+							<svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+								<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+							</svg>
+							<span class="sr-only">Info</span>
+							<div>
+								{{$message}}
+							</div>
+						</div>
+						@enderror
+					</div>
+
+					<div>
+						<label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+						<div class="mt-2">
+							<input id="email" name="email" type="email" autocomplete="email" placeholder="Enter email address" required value="{{old('email')}}" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6">
+						</div>
+						@error('email')
+						<div class="flex items-center p-4 mt-1 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+							<svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+								<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+							</svg>
+							<span class="sr-only">Info</span>
+							<div>
+								{{$message}}
+							</div>
+						</div>
+						@enderror
+					</div>
+
+					<div>
+						<div class="flex items-center justify-between">
+							<label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+							<div class="text-sm">
+								<a href="#" class="font-semibold text-sky-400 hover:text-sky-500">Forgot password?</a>
+							</div>
+						</div>
+						<div class="mt-2">
+							<input id="password" name="password" type="password" autocomplete="current-password" placeholder="Enter password" required value="{{old('password')}}" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6">
+						</div>
+						@error('password')
+						<div class="flex items-center p-4 mt-1 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+							<svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+								<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+							</svg>
+							<span class="sr-only">Info</span>
+							<div>
+								{{$message}}
+							</div>
+						</div>
+						@enderror
+					</div>
+
+					<div>
+						<button type="submit" class="flex w-full justify-center rounded-md bg-sky-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400">Login</button>
+					</div>
+				</form>
+
+				<p class="mt-2 text-center text-sm text-gray-500">
+					Don't have an account?
+					<a href="/register" class="font-semibold leading-6 text-sky-400 hover:text-sky-500">Sign up</a>
+				</p>
+			</div>
+		</div>
+</main>
+</body>
+
+</html>
--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -1,80 +1,151 @@
-<h1>Register</h1>
-<form method="POST" action="/register">
-            @csrf
-            <div class="mb-6">
-                <label for="firstName" class="inline-block text-lg mb-2">First Name</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="firstName" placeholder="First Name" value="{{old('firstName')}}" />
-                @error('firstName')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
-            </div>
+<!DOCTYPE html>
+<html lang="en">
 
-            <div class="mb-6">
-                <label for="lastName" class="inline-block text-lg mb-2">Last Name</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="lastName" placeholder="Last Name" value="{{old('lastName')}}" />
-                @error('lastName')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
-            </div>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+  <title>Sign up</title>
+</head>
 
-            <div class="mb-6">
-                <label for="email" class="inline-block text-lg mb-2">Email</label>
-                <input type="email" class="border border-gray-200 rounded p-2 w-full" name="email" placeholder="Other Names" value="{{old('email')}}" />
-                @error('email')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
-            </div>
+<body class="h-full">
+  <main class="flex min-h-full flex-col justify-center px-6 py-8 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm border rounded-xl py-12 px-6">
 
-            <div class="mb-6">
-                <label for="role" class="inline-block text-lg mb-2">Role</label>
-                <select name="role" id="role" class="border border-gray-200 rounded p-2 w-full">
-                    <option value="">--Choose role --</option>
-                    <option value="admin">Admin</option>
-                    <option value="lecturer">Lecturer</option>
-                </select>
-                @error('role')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
-            </div>
 
-            <div class="mb-6">
-                <label for="phoneNumber" class="inline-block text-lg mb-2">Phone Number</label>
-                <input type="number" class="border border-gray-200 rounded p-2 w-full" name="phoneNumber" placeholder="Registration Number" value="{{old('phoneNumber')}}" />
-                @error('phoneNumber')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
-            </div>
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img class="mx-auto h-16 w-auto" src={{asset("avatar.png")}} alt="Your Company">
+        <h2 class="mt-2 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign up</h2>
+      </div>
 
-            <div class="mb-6">
-                <label for="password" class="inline-block text-lg mb-2">
-                    Password
-                </label>
-                <input type="password" class="border border-gray-200 rounded p-2 w-full" name="password" placeholder="password" value="{{old('password')}}" />
-                @error('password')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
+      <div class="mt-4 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form class="space-y-6" action="/register" method="POST">
+          @csrf
+          <div>
+            <label for="role" class="block text-sm font-medium leading-6 text-gray-900">Role</label>
+            <div class="mt-2">
+              <select name="role" id="role" class="block w-full rounded-md border-0 py-1.5 px-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6">
+                <option value="">-- select role --</option>
+                <option value="admin">Admin</option>
+                <option value="lecturer">Lecturer</option>
+              </select>
             </div>
+            @error('role')
+            <div class="flex items-center p-4 mt-1 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+              <svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+              </svg>
+              <span class="sr-only">Info</span>
+              <div>
+                {{$message}}
+              </div>
+            </div>
+            @enderror
+          </div>
 
-            <div class="mb-6">
-                <label for="password_confirmation" class="inline-block text-lg mb-2">
-                    Confirm Password
-                </label>
-                <input type="password" class="border border-gray-200 rounded p-2 w-full" name="password_confirmation" value="{{old('password_confirmation')}}" />
-                @error('password_confirmation')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-                @enderror
+          <div>
+            <label for="firstName" class="block text-sm font-medium leading-6 text-gray-900">First Name</label>
+            <div class="mt-2">
+              <input id="firstName" name="firstName" type="text" autocomplete="firstName" placeholder="Enter first name" required value="{{old('firstName')}}" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6">
             </div>
+            @error('firstName')
+            <div class="flex items-center p-4 mt-1 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+              <svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+              </svg>
+              <span class="sr-only">Info</span>
+              <div>
+                {{$message}}
+              </div>
+            </div>
+            @enderror
+          </div>
 
-            <div class="mb-6">
-                <button class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
-                    Sign Up
-                </button>
+          <div>
+            <label for="lastName" class="block text-sm font-medium leading-6 text-gray-900">Last Name</label>
+            <div class="mt-2">
+              <input id="lastName" name="lastName" type="text" autocomplete="lastName" placeholder="Enter last name" required value="{{old('lastName')}}" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6">
             </div>
+            @error('lastName')
+            <div class="flex items-center p-4 mt-1 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+              <svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+              </svg>
+              <span class="sr-only">Info</span>
+              <div>
+                {{$message}}
+              </div>
+            </div>
+            @enderror
+          </div>
 
-            <div class="mt-8">
-                <p>
-                    Already have an account?
-                    <a href="/login" class="text-laravel">Login</a>
-                </p>
+          <div>
+            <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+            <div class="mt-2">
+              <input id="email" name="email" type="email" autocomplete="email" placeholder="Enter email address" required value="{{old('email')}}" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6">
             </div>
+            @error('email')
+            <div class="flex items-center p-4 mt-1 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+              <svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+              </svg>
+              <span class="sr-only">Info</span>
+              <div>
+                {{$message}}
+              </div>
+            </div>
+            @enderror
+          </div>
+
+          <div>
+            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+            <div class="mt-2">
+              <input id="password" name="password" type="password" autocomplete="current-password" placeholder="Enter password" required value="{{old('password')}}" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6">
+            </div>
+            @error('password')
+            <div class="flex items-center p-4 mt-1 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+              <svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+              </svg>
+              <span class="sr-only">Info</span>
+              <div>
+                {{$message}}
+              </div>
+            </div>
+            @enderror
+          </div>
+
+          <div>
+            <label for="password_confirmation" class="block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
+            <div class="mt-2">
+              <input id="password" name="password" type="password" autocomplete="current-password" placeholder="Enter password" required value="{{old('password')}}" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6">
+            </div>
+            @error('password_confirmation')
+            <div class="flex items-center p-4 mt-1 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+              <svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+              </svg>
+              <span class="sr-only">Info</span>
+              <div>
+                {{$message}}
+              </div>
+            </div>
+            @enderror
+          </div>
+
+          <div>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-sky-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400">Sign up</button>
+          </div>
         </form>
+
+        <p class="mt-2 text-center text-sm text-gray-500">
+          Already have an account?
+          <a href="/login" class="font-semibold leading-6 text-sky-400 hover:text-sky-500">Login</a>
+        </p>
+      </div>
+    </div>
+  </main>
+</body>
+
+</html>
