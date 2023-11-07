64edcac374dfabcb4a47ac4dbce94d5fbd360589
aladdin4u <taofikakinkumi@gmail.com>
2023-11-07 11:17:46 +0100

    move card element to components

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -5,7 +5,7 @@
 ])
 <div class="flex items-center justify-between px-4 py-3 rounded-lg bg-white dark:bg-gray-800">
     <div class="space-y-4">
-        <h3 class="font-sm text-gray-500">{{$title}}</h3>
+        <h3 class="text-sm text-gray-500">{{$title}}</h3>
         <h4 class="font-medium">{{$desc}}</h4>
     </div>
     <div class="flex items-center justify-center p-4 rounded-full bg-sky-500 text-white">
