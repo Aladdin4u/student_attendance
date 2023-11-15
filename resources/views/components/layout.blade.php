dd4bcfeca622d2d5898923784de97aac34420750
aladdin4u <taofikakinkumi@gmail.com>
2023-11-15 08:31:06 +0100

    added css for datatables

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,13 +10,12 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
     @vite('resources/css/app.css')
-    @vite('resources/sass/app.scss')
+    <!-- @vite('resources/sass/app.scss') -->
     <title>Attendify</title>
 </head>
 
