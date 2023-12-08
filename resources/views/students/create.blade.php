da16ce6f7da4ca9f7f3c0b2c4eb4b9c7110dbc54
aladdin4u <taofikakinkumi@gmail.com>
2023-12-08 08:00:44 +0100

    mobile responsive

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -54,7 +54,7 @@
                 @enderror
             </div>
 
-            <div class="mx-auto flex flex-row items-center justify-between">
+            <div class="mx-auto flex flex-row items-center justify-between space-x-2">
                 <button type="submit" class="flex w-60 justify-center rounded-md bg-sky-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400">
                     Save
                 </button>
