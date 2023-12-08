6f67b90488ce0beca2b0d008d21443fcb986ee0d
aladdin4u <taofikakinkumi@gmail.com>
2023-12-08 07:32:01 +0100

    mobile responsive

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -30,7 +30,7 @@
                                 <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                             </svg>
                         </button>
-                        <div x-show="open" class="absolute w-64 h-screen bg-white top-16">
+                        <div x-show="open" class="absolute left-0 mt-2.5 w-64 h-screen bg-white">
                             @include("partials._sidebar")
                         </div>
                     </div>
@@ -95,7 +95,7 @@
         </div>
     </nav>
     <main>
-        <div class="hidden md:block">
+        <div class="hidden md:block fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700">
             @include("partials._sidebar")
         </div>
         <div class="p-4 sm:ml-64 bg-gray-100">
