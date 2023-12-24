0e5143ee80492e268ca20703379ebf31525c6fa9
aladdin4u <taofikakinkumi@gmail.com>
2023-12-24 23:57:46 +0100

    add packeage to resources

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,13 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="//unpkg.com/alpinejs" defer></script>
-    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
-    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.7.0.js"></script> -->
+    <!-- <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script> -->
+    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script> -->
+    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css','resources/css/daterangepicker.css', 'resources/js/app.js', 'resources/js/jquery.js', 'resources/js/datatables.min.js', 'resources/js/moment.min.js', 'resources/js/daterangepicker.min.js', 'resources/js/chart.js'])
     <title>Attendify</title>
 </head>
 
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,13 @@ export default defineConfig({
         laravel({
             input: [
                 "resources/css/app.css",
+                "resources/css/daterangepicker.css",
                 "resources/js/app.js",
+                "resources/js/jquery.js",
+                "resources/js/datatables.min.js",
+                "resources/js/moment.min.js",
+                "resources/js/daterangepicker.min.js",
+                "resources/js/chart.js",
             ],
             refresh: true,
         }),
